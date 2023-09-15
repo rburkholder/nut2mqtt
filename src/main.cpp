8b445b7a221ff009f2f656e4ef54d5835dfc9396
Raymond Burkholder <raymond@burkholder.net>
2023-09-14 20:47:07 -0600

    src/main.cpp - try building with nut library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 
 #include <iostream>
 
-int main() {
+// requires apt install libnutclient-dev
+#include <nutclient.h>
+
+int main( int argc, char **argv ) {
   std::cout << "hello" << std::endl;
+
+  nut::Client *client;
+  client = new nut::TcpClient("localhost", 3493);
+
+  nut::Device mydev = client->getDevice(argv[1]);
+  std::cout << "Description: " << mydev.getDescription() << std::endl;
+  nut::Variable var = mydev.getVariable("device.model");
+  std::cout << "Model: " <<  var.getValue()[0] << std::endl;
+
+  delete client;
+
   return 0;
 }
 
